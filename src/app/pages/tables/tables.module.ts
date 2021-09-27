43e4ad8b8b5861e4ec018dceff574420af7e09d4
Axel <achepo@gmail.com>
2021-09-27 10:43:22 -0300

    husky

--- src/app/pages/tables/tables.module.ts
+++ src/app/pages/tables/tables.module.ts
@@ -12,8 +12,8 @@ import { DatatableComponent } from './datatable/datatable.component';
 
 /* https://github.com/HTMLElements/smart-webcomponents */
 import { TableModule } from 'smart-webcomponents-angular/table';
- 
- /* https://l-lin.github.io/angular-datatables/#/welcome */  
+
+/* https://l-lin.github.io/angular-datatables/#/welcome */
 import { DataTablesModule } from 'angular-datatables';
 
 import { ModalEditComponent } from './modal-edit/modal-edit.component';
@@ -32,7 +32,7 @@ import { DatatableAngularComponent } from './datatable-angular/datatable-angular
     NbInputModule,
     ThemeModule,
     TablesRoutingModule,
-    Ng2SmartTableModule, 
+    Ng2SmartTableModule,
     TableModule,
     DataTablesModule,
     NgbModule,
